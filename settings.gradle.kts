b03971462a3801864d706662ffb02b11a4718dd0
Ryan Leach <leachryan@Ryans-MacBook-Pro.local>
2023-10-08 20:57:56 -0500

    Initial commit for project repo

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "harpocrates-web-backend"
---
+++ src/test/kotlin/dev/leachryan/harpocrateswebbackend/HarpocratesWebBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.leachryan.harpocrateswebbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HarpocratesWebBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
